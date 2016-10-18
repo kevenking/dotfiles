f4a53c7db6c4a18838babfe2af600164994e8a26
kevenking <kevenking@gmail.com>
2016-10-18 16:17:00 +0800

    修改环境变量和配置文件

--- docker/path.zsh
+++ docker/path.zsh
@@ -1,3 +1,4 @@
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=/Users/amowu/.boot2docker/certs/boot2docker-vm
-export DOCKER_TLS_VERIFY=1
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://192.168.99.100:2376"
+export DOCKER_CERT_PATH="/Users/mac/.docker/machine/machines/default"
+export DOCKER_MACHINE_NAME="default"
